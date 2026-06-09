09963b8a040b38d2f18f1e7a726c77132348faf7
space <yuma.dark.black@gmail.com>
2026-06-09 15:31:45 +0900

    entrypoint: composer install を chown より先に実行し権限問題を解消

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 cd /var/www/html
 
+composer install --no-interaction
+
 if [ ! -f .env ]; then
   cp .env.example .env
   php artisan key:generate
 fi
-
-composer install --no-interaction
 
 chown -R www-data:www-data storage bootstrap/cache
 
